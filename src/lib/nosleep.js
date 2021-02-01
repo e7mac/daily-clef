556aa254949858203387c0303441e848298d4e45
Mayank Sanganeria <mayank.ot@gmail.com>
2021-01-31 21:22:38 -0800

    lint

--- src/lib/nosleep.js
+++ src/lib/nosleep.js
@@ -110,8 +110,6 @@
 /* 0 */
 /***/ (function (module, exports, __webpack_require__) {
 
-      "use strict";
-
 
       var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -264,7 +262,6 @@
 /* 1 */
 /***/ (function (module, exports, __webpack_require__) {
 
-      "use strict";
 
 
       module.exports = {
