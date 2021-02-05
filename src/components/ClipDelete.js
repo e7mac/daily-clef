2e3f7b4aab735db347fd91059537c87625484f43
Mayank Sanganeria <mayank.ot@gmail.com>
2021-02-05 08:22:16 -0800

    add files

---
+++ src/components/ClipDelete.js
@@ -0,0 +1,34 @@
+import { Container, Button, Row, Col, Form } from 'react-bootstrap';
+import React from 'react';
+
+import './ClipDelete.css';
+
+export default class ClipDelete extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			confirmation: false,
+			deleted: false
+		}
+	}
+
+	deleteClip = () => {
+		this.props.api.deleteClip(this.props.clip.id).then(() => {
+			this.setState({ deleted: true })
+		})
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				{
+					this.state.deleted
+						? "Deleted. Refresh as needed"
+						: this.state.confirmation
+							? <Button variant="danger" onClick={this.deleteClip}>Sure?</Button>
+							: <Button variant="info" onClick={() => { this.setState({ confirmation: true }) }}>Delete</Button>
+				}
+			</React.Fragment>
+		);
+	}
+}
