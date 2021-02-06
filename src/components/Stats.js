9622e1a74bd251fbbaab7ad85540df39491d1afb
Mayank Sanganeria <mayank.ot@gmail.com>
2021-02-05 16:28:53 -0800

    add charts

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,6 +3,8 @@ import React from 'react';
 import * as TimeFormatUtils from '../utils/TimeFormatUtils'
 import './Stats.css';
 
+import { XAxis, YAxis, Legend, CartesianGrid, BarChart, Bar, PieChart, Pie, Tooltip } from 'recharts';
+
 export default class Stats extends React.Component {
 	constructor(props) {
 		super(props)
@@ -18,16 +20,38 @@ export default class Stats extends React.Component {
 		})
 	}
 
+	format(value, name, props) {
+		return TimeFormatUtils.formatDuration(value)
+	}
+
 	render() {
+		let data = []
+		if (this.state.stats) {
+			for (const item of this.state.stats.labels) {
+				data.push({
+					name: item.label,
+					value: item.duration
+				})
+			}
+		}
+		data.sort((a, b) => b.value - a.value)
 		return (
 			<React.Fragment>
 				{this.state.stats
 					? <React.Fragment>
 						<p>{`Total time practiced: ${TimeFormatUtils.formatDuration(this.state.stats.duration)}`}</p>
-						{this.state.stats.labels.map((label) => {
-							return <p>{label.label}: {TimeFormatUtils.formatDuration(label.duration)}</p>
-						})
-						}
+						<BarChart width={1200} height={400} data={data} barCategoryGap={1} barSize={30} >
+							<CartesianGrid strokeDasharray="3 3" />
+							<XAxis dataKey="name" />
+							<YAxis tickFormatter={this.format} />
+							<Legend />
+							<Tooltip formatter={this.format} />
+							<Bar dataKey="value" fill="#8884d8" />
+						</BarChart>
+						<PieChart width={400} height={400}>
+							<Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={200} />
+							<Tooltip formatter={this.format} />
+						</PieChart>
 					</React.Fragment>
 					: "Loading..."
 				}
