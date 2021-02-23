a2f0cb7fd3511e0392eca199ab127a32b21a8aae
Mayank Sanganeria <mayank.ot@gmail.com>
2021-02-22 23:06:26 -0800

    simple pdf view

--- src/components/PDFDisplay.js
+++ src/components/PDFDisplay.js
@@ -1,82 +1,9 @@
-import { Button } from 'react-bootstrap';
-import { Document, Page, pdfjs } from 'react-pdf';
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 export default function PDFDisplay(props) {
-	const [numPages, setNumPages] = useState(null);
-	const [pageNumber, setPageNumber] = useState(1);
-	const handle = useFullScreenHandle();
-
-	useEffect(() => {
-		// Runs ONCE after initial rendering
-		pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-	}, []);
-
-	function onDocumentLoadSuccess({ numPages }) {
-		setNumPages(numPages);
-		setPageNumber(1);
-	}
-
-	function changePage(offset) {
-		setPageNumber(prevPageNumber => prevPageNumber + offset);
-	}
-
-	function previousPage() {
-		changePage(-1);
-	}
-
-	function nextPage() {
-		changePage(1);
-	}
-
-	function pdfPage() {
-		return (
-			<>
-				<div>
-					<div>
-						<p>
-							Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-						</p>
-						<Button
-							variant="light"
-							disabled={pageNumber <= 1}
-							onClick={previousPage}>
-							Previous
-						</Button>
-						<Button
-							variant="light"
-							disabled={pageNumber >= numPages}
-							onClick={nextPage}>
-							Next
-						</Button>
-					</div>
-					<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-						<Document
-							file={props.file}
-							onLoadSuccess={onDocumentLoadSuccess}
-						>
-							<Page pageNumber={pageNumber} />
-						</Document>
-					</div>
-				</div>
-			</>
-		)
-	}
-
 	return (
 		<>
-			<div>
-				<Button
-					variant="light"
-					onClick={handle.enter}>
-					Enter fullscreen
-				</Button>
-				<FullScreen handle={handle}>
-					{pdfPage()}
-				</FullScreen>
-			</div>
-			{pdfPage()}
+			<iframe title="Sheet music" src={`https://docs.google.com/viewer?embedded=true&url=${props.file}`} width="100%" height="900px" />
 		</>
 	);
 }
