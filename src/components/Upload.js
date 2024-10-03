3cd76197dae60c2caa2d4cc46c7be68f59f27679
Mayank Sanganeria <mayank.ot@gmail.com>
2024-10-03 00:46:33 -0400

    update form file for bootstrap

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -4,35 +4,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Upload(props) {
+  const fileRef = useRef(null)
 
-	const fileRef = useRef(null)
+  const submitClicked = (e) => {
+    const fileInput = fileRef.current
+    const files = fileInput.files;
+    for (let file of files) {
+      const modified = Math.floor(file.lastModified / 1000);
+      props.setStatus("Uploading...")
+      props.api.uploadFileFlow(file, modified)
+        .then(() => {
+          fileInput.value = ''
+          props.setStatus("✅ Uploaded")
+        })
+    }
+  }
 
-	// const submitClicked = (e) => {
-	// 	const fileInput = fileRef.current
-	// 	const files = fileInput.files;
-	// 	for (let file of files) {
-	// 		const modified = Math.floor(file.lastModified / 1000);
-	// 		props.setStatus("Uploading...")
-	// 		props.api.uploadFileFlow(file, modified)
-	// 			.then(() => {
-	// 				fileInput.value = ''
-	// 				props.setStatus("✅ Uploaded")
-	// 			})
-	// 	}
-	// }
+  const upload = (e) => {
+    fileRef.current.click()
+  }
 
-	const upload = (e) => {
-		fileRef.current.click()
-	}
-
-	return (
-		<span>
-			<Form hidden={true}>
-				<Form.Group>
-					{/* <Form.File onChange={submitClicked} ref={fileRef} accept="audio/*, video/*" /> */}
-				</Form.Group>
-			</Form>
-			<Button variant="outline-success" onClick={upload}><FontAwesomeIcon icon={faCloudUploadAlt} /></Button>
-		</span>
-	);
+  return (
+    <span>
+      <Form.Group controlId="formFile" className="mb-3" hidden>
+        <Form.Control type="file" onChange={submitClicked} ref={fileRef} accept="audio/*, video/*" />
+      </Form.Group>
+      <Button variant="outline-success" onClick={upload}>
+        <FontAwesomeIcon icon={faCloudUploadAlt} />
+      </Button>
+    </span>
+  );
 }
